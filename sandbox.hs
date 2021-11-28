d948539b4ea73dfd3c57a0f64e14ba414b6139b7
444b <444b+github@protonmail.ch>
2021-11-28 16:32:19 +0200

    test

---
+++ sandbox.hs
@@ -0,0 +1,19 @@
+-- Dependencies
+import Data.List ()
+import System.IO ()
+
+x :: Integer
+x = 4
+
+y :: [Char]
+y = "Dylan"
+
+z :: Double
+z = 1.5
+
+
+a :: (Integer, Integer, Integer, Integer, Integer)
+a = (1,2,2,3,4)
+
+c :: Bool 
+c = True 
