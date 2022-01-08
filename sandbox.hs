275df3d5e788662be34b58fcd0fc6fc93b0a565f
444B <444B+github@protonmail.ch>
2022-01-08 12:42:46 +0200

    test

--- sandbox.hs
+++ sandbox.hs
@@ -1,22 +1,47 @@
 double :: Num a => a -> a
 double x = x + x
 
+double2 :: Num a => a -> a -> a
 double2 x y = x * 2 + y * 2
 
+doubleSmallerNumber :: (Ord a, Num a) => a -> a
 doubleSmallerNumber x =
   if x > 100
     then x
     else x * 2
 
-conanO'Brien = "It's a-me, Conan O'Brien!"
-
+lostNumbers :: [Integer]
 lostNumbers = [8, 15, 16, 23, 42]
 
+a :: [a]
 a = []
 
+b :: [Integer]
 b = [5, 4, 3, 2, 1]
 
+conan :: [Char]
+conan = "It's a-me, Conan O'Brien!"
+
+empty :: Foldable t => t a -> IO ()
 empty x =
   if null x == True
     then print "fff"
     else print "sss"
+
+slice :: [Char]
+slice = take 4 conan
+
+sliceoppositedirec :: [Integer]
+sliceoppositedirec = drop 3 [8, 4, 2, 1, 5, 6]
+
+scan :: (Eq a, Num a) => a -> Bool
+scan n = n `elem` [5, 4, 3, 2, 1]
+
+letters :: [Char]
+letters = ['a' .. 'z']
+
+numbers :: [Integer]
+numbers = [1 .. 20]
+
+
+loopy = cycle (["kek "])
