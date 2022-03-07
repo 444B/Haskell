8610f4142d44cb817bc6a855e21cd17779ca115c
444B <444B+github@protonmail.ch>
2022-03-07 23:31:49 +0200

    homework

--- test.hs
+++ test.hs
@@ -1,5 +1,8 @@
 sayHello :: String -> IO ()
 sayHello x = putStrLn ("Hello, " ++ x ++ "!")
-
 triple :: Num a => a -> a
 triple x = x * 3
+double :: Num a => a -> a
+double x = x * 2
+perimeter x y = x*2+y*2
+f x = x / (2 + 9)
