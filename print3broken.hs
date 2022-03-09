b0762982e8629ca5cc30f8c54958166e864392cc
444B <444B+github@protonmail.ch>
2022-03-10 00:16:52 +0200

    More progress :)

---
+++ print3broken.hs
@@ -0,0 +1,12 @@
+-- print3broken.hs
+module Print3Broken where
+
+    greeting = "Yarrrr!"
+    printSecond :: IO ()
+    printSecond = do
+      putStrLn greeting 
+
+    main :: IO ()
+    main = do
+      putStrLn greeting
+      printSecond
