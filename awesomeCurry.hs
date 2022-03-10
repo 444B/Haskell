7df556245dfb572a2fa815e57e67e39b23a89a98
444B <444B+github@protonmail.ch>
2022-03-11 01:16:02 +0200

    sync for weekend work

--- awesomeCurry.hs
+++ awesomeCurry.hs
@@ -1,6 +1,13 @@
 module AwesomeCurry where
-    string :: [Char]
-    string = "Curry is awesome!"
 
-    addExclamation :: [Char] -> [Char]
-    addExclamation x = x ++ "!"
+string :: [Char]
+string = "Curry is awesome!"
+
+addExclamation :: [Char] -> [Char]
+addExclamation x = x ++ "!"
+
+targets :: [Char]
+targets = "Curry is awesome"
+
+rvrs :: [Char] -> [Char]
+rvrs x = (drop 9 x) ++ " " ++ (take 3 (drop 6 x)) ++ (take 5 x)
